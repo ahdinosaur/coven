e1edb77455dba67157408f0f2235d89bd4634f6e
Michael Williams <michael.williams@enspiral.com>
2023-10-29 01:44:10 +1300

    yee haw

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,10 @@ pub mod time;
 use cable_core::MemoryStore;
 use dioxus::prelude::*;
 use dioxus_router::prelude::*;
-use fermi::{use_init_atom_root, use_read};
+use fermi::{use_init_atom_root, use_read, use_set};
 
 use crate::service::{create_service, Command};
-use crate::state::CABAL_IDS;
+use crate::state::{ACTIVE_CABAL_CHANNEL_ID, ACTIVE_CABAL_ID, CABAL_CHANNEL_IDS, CABAL_IDS};
 
 #[derive(Routable, Clone)]
 #[rustfmt::skip]
@@ -90,14 +90,23 @@ fn CabalCardList(cx: Scope, cabal_ids: Vec<String>) -> Element {
 #[inline_props]
 fn CabalLayout(cx: Scope) -> Element {
     let cabal_ids = use_read(cx, &CABAL_IDS);
+    let active_cabal_id = use_read(cx, &ACTIVE_CABAL_ID);
+    let channel_ids = use_read(cx, &CABAL_CHANNEL_IDS);
+    let _active_channel_id = use_read(cx, &ACTIVE_CABAL_CHANNEL_ID);
 
     render! {
         nav {
             Link {
-                to: Route::AppHomePage { },
+                to: Route::AppHomePage {},
                 "Home"
             },
-            CabalNavList { cabal_ids: cabal_ids.clone() }
+            CabalNavList { cabal_ids: cabal_ids.clone() },
+            if active_cabal_id.is_some() && channel_ids.is_some() {
+                rsx!(CabalChannelList {
+                    cabal_id: active_cabal_id.clone().unwrap(),
+                    channel_ids: channel_ids.clone().unwrap(),
+                })
+            }
         }
         Outlet::<Route> {}
     }
@@ -109,6 +118,7 @@ fn CabalNavList(cx: Scope, cabal_ids: Vec<String>) -> Element {
         ul {
             for cabal_id in cabal_ids {
                 li {
+                    key: "{cabal_id}",
                     Link {
                         to: Route::CabalHomePage { cabal_id: cabal_id.into() },
                         "{cabal_id}",
@@ -120,7 +130,29 @@ fn CabalNavList(cx: Scope, cabal_ids: Vec<String>) -> Element {
 }
 
 #[inline_props]
+fn CabalChannelList(cx: Scope, cabal_id: String, channel_ids: Vec<String>) -> Element {
+    render! {
+        ul {
+            for channel_id in channel_ids {
+                li {
+                    key: "{channel_id}",
+                    Link {
+                        to: Route::CabalChannelPage {
+                            cabal_id: cabal_id.into(),
+                            channel_id: channel_id.into()
+                        },
+                        "{channel_id}",
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[inline_props]
 fn CabalHomePage(cx: Scope, cabal_id: String) -> Element {
+    use_active_cabal_id(cx, cabal_id.into());
+
     render! {
         div {
             h1 {
@@ -132,9 +164,44 @@ fn CabalHomePage(cx: Scope, cabal_id: String) -> Element {
 
 #[inline_props]
 fn CabalChannelPage(cx: Scope, cabal_id: String, channel_id: String) -> Element {
+    use_active_cabal_id(cx, cabal_id.into());
+    use_active_channel_id(cx, channel_id.into());
+
     render! {
         div {
             "Cabal Channel"
         }
     }
+}
+
+fn use_active_cabal_id(cx: &ScopeState, cabal_id: String) {
+    let set_active_cabal_id = use_set(cx, &ACTIVE_CABAL_ID);
+    use_effect(cx, (), |_| {
+        to_owned![set_active_cabal_id];
+        async move {
+            set_active_cabal_id(Some(cabal_id));
+        }
+    });
+    use_on_unmount(cx, {
+        to_owned![set_active_cabal_id];
+        move || {
+            set_active_cabal_id(None);
+        }
+    });
+}
+
+fn use_active_channel_id(cx: &ScopeState, channel_id: String) {
+    let set_active_channel_id = use_set(cx, &ACTIVE_CABAL_CHANNEL_ID);
+    use_effect(cx, (), |_| {
+        to_owned![set_active_channel_id];
+        async move {
+            set_active_channel_id(Some(channel_id));
+        }
+    });
+    use_on_unmount(cx, {
+        to_owned![set_active_channel_id];
+        move || {
+            set_active_channel_id(None);
+        }
+    });
 }
