149b165cf46b4781cdd4691d7952aa9b4acb92d7
Michael Williams <michael.williams@enspiral.com>
2023-11-01 02:18:07 +1300

    attempt to optimize tailwind, but nah

--- Justfile
+++ Justfile
@@ -1,2 +1,14 @@
 dev:
   dx serve --hot-reload
+
+expand:
+  cargo expand --lib > ./dist/expanded.rs
+
+expand_watch:
+  watchexec -e rs cargo expand --lib
+
+tailwind: expand
+  npm run tailwind
+
+tailwind_watch: expand_watch
+  npm run tailwind:watch
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./dist/expanded.rs"],
   theme: {
     extend: {},
   },
-  safelist: [
-    // HACK: render ALL tailwind and daisyui classes
-    // TODO: use build.rs to render dioxus app to html in dist, then pass dist into tailwind to check classes
-    { pattern: /./ },
-  ],
   plugins: [require("daisyui")],
 };
